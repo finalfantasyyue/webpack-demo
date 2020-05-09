6468b9efdeb805eb966f726c3fee4041dff748c9
zhangdawei <zhangdawei@p2peye.com>
2020-05-09 14:19:06 +0800

    增加file-loader,url-loader,配置字体和图片

--- src/search.js
+++ src/search.js
@@ -2,11 +2,15 @@
 
 import React from 'react'
 import ReactDom from 'react-dom'
+import browser from './assets/images/brower.jpg'
 import './index.less'
 
 class Search extends React.Component {
   render() {
-    return <div class="search-text">Search Text</div>
+    return <div class="search-text">
+      Search Text
+      <img src={ browser } alt="browser"/>
+    </div>
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,35 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         use: 'babel-loader'
       },
       {
-        test: /.(css|less)$/,
+        test: /\.(css|less)$/,
         use: ['style-loader', 'css-loader', 'less-loader'] // 从右到左执行，先用less-loader转换成css,再用css-loader把css打包到commonjs里，在通过style-loader将css写到style标签里
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]',
+              limit: 10240 // 图片小于10k时，会自动打包到js里
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|otf|ttf|eot|TTC)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        ]
       }
     ]
   },
