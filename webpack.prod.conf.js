20680e7d2e8ebfde35fe251a6cf3546bb4074559
zhangdawei <zhangdawei@p2peye.com>
2020-05-09 19:00:40 +0800

    增加css压缩，html对应的css,js打包到对应的html里

--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -1,7 +1,8 @@
 'use strict'
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 从js里抽离出css，css单独是个文件
+const OptimizeCssAssetsWebpackPlugin  = require('optimize-css-assets-webpack-plugin') // css压缩
 const path = require('path')
 const webpack = require('webpack')
 module.exports = {
@@ -57,11 +58,24 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html'
+      // template: './src/index.html',
+      template: path.resolve(__dirname, './src/index.html'),
+      chunks: ['index'], // 将index.css,index.js打包到index.html里
+      inject: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'search.html',
+      template: path.resolve(__dirname, './src/search.html'),
+      chunks: ['search'], // 将search.js，search.css打包到search.html里
+      inject: true
     }),
     new CleanWebpackPlugin(), // 每次重新打包时都会清空构建目录（dist）
     new MiniCssExtractPlugin({
       filename: '[name]_[contenthash:8].css'
-    }) // 从js里抽离出css，css单独是个文件, css用contenthash
+    }), // 从js里抽离出css，css单独是个文件, css用contenthash
+    new OptimizeCssAssetsWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano')
+    })
   ]
 }
